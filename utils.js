750652b6da27ab3a08f4f612d5cf5a5f35c0c212
Marcos Loureiro <mloureiro.dev@gmail.com>
2022-04-05 17:01:55 +0200

    Add simple utility methods to help with the shape setup

---
+++ utils.js
@@ -0,0 +1,24 @@
+/**
+ * Generate an array with requested range
+ * @param  {number} min
+ * @param  {number} max
+ * @return {Array.<number>}
+ */
+export const range = (min, max) => Array(max - min).fill(0).map((_, i) => min + i);
+
+/**
+ * Get a random element form the array
+ * @template I
+ * @param    {Array.<I>} list
+ * @return   {I}
+ */
+export const randomItem = list => list[Math.floor(Math.random() * list.length)];
+
+/**
+ * Ensures it returns an item from the list
+ * @template I
+ * @param    {I} [item]
+ * @param    {Array.<I>}list
+ * @return   {I}
+ */
+export const itemOrRandom = (item, list) => item && list.includes(item) ? item : randomItem(list);
