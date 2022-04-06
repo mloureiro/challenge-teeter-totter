0e19121652788393190206f5896dfc5979615094
Marcos Loureiro <mloureiro.dev@gmail.com>
2022-04-06 07:38:27 +0200

    Install Vuex store

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 import Playground from './playground.vue'
+import { store } from './store'
 import './main.scss';
 
-createApp(Playground).mount('#app')
+const app = createApp(Playground);
+
+app.use(store);
+app.mount('#app');
