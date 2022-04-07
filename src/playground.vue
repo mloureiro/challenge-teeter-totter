ff32802da571cb5529c3abb0746614da8f59653c
Marcos Loureiro <mloureiro.dev@gmail.com>
2022-04-08 00:58:00 +0200

    Calculate and print the weights in the playground

--- src/playground.vue
+++ src/playground.vue
@@ -1,25 +1,127 @@
 <template>
-	<div class="playground">
-		<scale :deviation="deviation" />
+	<div ref="wrapper" class="playground __wrapper">
+		<div
+			v-for="(weight, index) in weightList"
+			:key="index"
+			:style="weight.style"
+			class="playground __weight"
+		>
+			<weight
+				:weight="weight.value"
+				:shape="weight.shape"
+			/>
+		</div>
+		<scale
+			:bending="bending"
+			@board="board = $event"
+		/>
 	</div>
 </template>
 
 <script>
+import { mapState, mapMutations } from 'vuex';
 import Scale from './components/scale.vue';
-import Shape from './components/shape.vue';
+import Weight from './components/weight.vue';
+import { calculateYInLinearEquation } from './utils';
+import { MUTATIONS, GAME_CONFIGURATION } from './store';
+
+/**
+ * @typedef {import('./store').State} State
+ * @typedef {import('./store').Weight} Weight
+ * @typedef {import('./store').Position} Position
+ */
+
+/**
+ * @augments {Weight}
+ * @typedef  {Object} PositionedWeight
+ * @property {Object.<string, string>} style
+ */
+
+/**
+ * @typedef {Object} BoardConfig
+ * @property {Position} start
+ * @property {Position} end
+ * @property {number} radAngle
+ */
+
+/**
+ * Calculate weight board position equivalent in playground
+ * @param   {Position} position
+ * @param   {Object} config
+ * @param   {number} config.totalWidthPositions
+ * @param   {number} config.totalHeightPositions
+ * @param   {Position} config.start
+ * @param   {Position} config.end
+ * @returns {Position}
+ */
+const calculatePositionWithinPlayground = (position, config) => {
+	const { totalWidthPositions, totalHeightPositions, start, end } = config;
+
+	// add +1 so that the middle of the board is empty
+	const widthPositions = totalWidthPositions + 1;
+	const slotWidth = Math.abs(start[0] - end[0]) / widthPositions;
+
+	const [x, y] = position;
+	// constructor side increment to avoid middle slot if position is on the right side
+	const sideIncrement = x >= (totalWidthPositions / 2) ? 1 : 0
+	const xWidth = (x + sideIncrement) * slotWidth;
+	const boardHeightAtX = calculateYInLinearEquation(xWidth, start, end);
+	const slotHeight = boardHeightAtX / totalHeightPositions;
+
+	return [(slotWidth / 2) + xWidth, y * slotHeight];
+};
 
 export default {
-	components: { Scale, Shape },
+	components: { Scale, Weight },
 	data: () => ({
-		deviation: 50,
+		/** @property {BoardConfig | null} */
+		board: null,
+		/** @property {number} */
+		playgroundHeight: 0,
+	}),
+	computed: mapState({
+		/**
+		 * Parse/compute game weights into respective UI weights
+		 * with the absolute position in the screen
+		 * @param   {State} state
+		 * @returns {Array.<PositionedWeight>}
+		 */
+		weightList(state) {
+			return state.list.map(weight => {
+				const [x, y] = this.calculateWeightPosition(weight);
+				return {
+					...weight,
+					style: {
+						left: `calc(${x}px + ${this.board?.radAngle} * 4rem)`,
+						top: `calc(${y}px + ${this.board?.radAngle} * 4rem)`,
+						transform: `rotate(${this.board?.radAngle}rad)`
+					},
+				};
+			});
+		},
+		bending(state, getters) {
+			return getters.bending;
+		},
 	}),
 	mounted() {
 		window.addEventListener('keydown', this.onKeyPress)
+		this.playgroundHeight = this.$refs.wrapper?.getBoundingClientRect().height || null;
 	},
 	unmounted() {
 		window.removeEventListener('keydown', this.onKeyPress)
 	},
 	methods: {
+		...mapMutations({
+			onMoveLeft: MUTATIONS.moveLeft,
+			onMoveRight: MUTATIONS.moveRight,
+			onPause: MUTATIONS.pause,
+			onPlay: MUTATIONS.play,
+		}),
+		/**
+		 * @param {Event} event
+		 *
+		 * TODO handle touch events for mobile
+		 */
 		onKeyPress(event) {
 			const action = {
 				ArrowRight: this.onMoveRight,
@@ -28,11 +130,23 @@ export default {
 
 			action?.();
 		},
-		onMoveRight() {
-			// TODO
-		},
-		onMoveLeft() {
-			// TODO
+		/**
+		 * @param   {Weight} weight
+		 * @returns {Position}
+		 */
+		calculateWeightPosition(weight) {
+			if (!this.board) return ['-9000px', '-9000px'];
+
+			return calculatePositionWithinPlayground(
+				weight.position,
+				{
+					// TODO define Vue component data properties properly (`this.board` type is not identified)
+					...(/** @type BoardConfig */this.board),
+					totalHeightPositions: GAME_CONFIGURATION.height,
+					totalWidthPositions: GAME_CONFIGURATION.width,
+					playgroundHeight: this.playgroundHeight,
+				},
+			);
 		},
 	},
 };
@@ -40,10 +154,17 @@ export default {
 
 <style scoped lang="scss">
 .playground {
-	height: 100vh;
-	width: 100vw;
-	padding: var(--space-s);
-	display: flex;
-	justify-content: center;
+	&.__wrapper {
+		height: 100%;
+		width: 100%;
+		position: relative;
+		padding: var(--space-s);
+		display: flex;
+		justify-content: center;
+	}
+
+	&.__weight {
+		position: absolute;
+	}
 }
 </style>
--- src/utils.js
+++ src/utils.js
@@ -37,3 +37,15 @@ export const randomItem = list => list[randomNumber(0, list.length - 1)];
  * @return {boolean}
  */
 export const isBetween = (n, [min, max]) => !Number.isNaN(n) && n >= min && n <= max;
+
+/**
+ * Calculate Y on linear equation, based on provided X
+ * @param   {number} x
+ * @param   {number} x1
+ * @param   {number} y1
+ * @param   {number} x2
+ * @param   {number} y2
+ * @returns {number}
+ */
+export const calculateYInLinearEquation = (x, [x1, y1], [x2, y2]) =>
+	(y2 - y1) / (x2 - x1) * x + (x2 * y1 - x1 * y2) / (x2 - x1);
