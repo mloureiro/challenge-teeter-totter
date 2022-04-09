df63c06c162e5d8e4e8fa70129f7a57a8004fa4c
Marcos Loureiro <mloureiro.dev@gmail.com>
2022-04-09 23:07:30 +0200

    Deploy game to GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader()]
+  base: '/challenge-teeter-totter/',
+  plugins: [vue(), svgLoader()],
 })
